c83153ecf15ffa9a9c190e5deb7deb8f7aa80e68
Elyor Shokirov <elton08@list.ru>
2025-02-03 11:53:40 +0200

    second commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
+
+import '@/styles/index.css'
+
 import App from './app'
 import AuthProvider from './components/providers/auth-provider'
 import { ThemeProvider } from './components/providers/theme-provider'
-import './index.css'
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
